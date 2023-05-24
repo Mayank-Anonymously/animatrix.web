35081d65c02823433a693c2a6a3f06a882c25e10
anubhav-midas <124804023+anubhav-midas@users.noreply.github.com>
2023-05-25 03:56:52 +0530

    Website Refreshed

--- components/_home/ImageBanner.js
+++ components/_home/ImageBanner.js
@@ -7,7 +7,7 @@ const Imagebanner = ({ imagebtn }) => {
       <div
         className="container-fluid image-banner"
         style={{
-          backgroundImage: `url("/resource/images/category.jpg")`,
+          backgroundImage: `url(${host}resources/category.jpg)`,
         }}
       >
         <div className="container">
