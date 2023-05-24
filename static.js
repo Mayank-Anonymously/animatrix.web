515c36b5603800ab23c934bc97a1328e1d4fba56
anubhav-midas <124804023+anubhav-midas@users.noreply.github.com>
2023-05-25 03:14:25 +0530

    API HOST CHANGE

--- static.js
+++ static.js
@@ -1,3 +1,2 @@
-export const host = "http://192.168.3.116:9000/";
-// export const host = "https://api.apisecommerce.com/";
-// export const host = "https://api.apisecommerce.com/";
+// export const host = "http://192.168.3.116:9000/";
+export const host = "https://api.apisecommerce.com/";
