01e85fafed684daf5cf801d1dcc55e6e4f845cfe
anubhav-midas <124804023+anubhav-midas@users.noreply.github.com>
2023-05-25 03:43:48 +0530

    Website Refreshed

--- components/product/list.js
+++ components/product/list.js
@@ -15,10 +15,6 @@ const List = () => {
     GetAllProduct({ setProductData });
   }, []);
 
-  const filteredData = productData.filter(
-    (item, index) => cd == item.categoryId
-  );
-
   return (
     <>
       <SecondBanner
@@ -32,41 +28,37 @@ const List = () => {
             "Wait while we fetch products for you"
           ) : (
             <>
-              {filteredData.length === 0 ? (
-                " No Products Found In The Catgory "
-              ) : (
-                <div className="row card-container">
-                  {filteredData.map((item, index) => {
-                    return (
-                      <>
-                        <div className="col-lg-4 col-md-8 col-sm-10 image-container">
-                          <div className="card-inner-container">
-                            <Link href={`/product/${item._id}`}>
-                              <img src={`${host}resources/${item.image}`} />
-                              {/* <img src="/resource/images/product-images/angle.jpeg" /> */}
-                              <span>
-                                <p className="tshirt-price">
-                                  {item.title} - {item.categoryName}
-                                </p>
-                                <span id="price">
-                                  <h5>Rs.{item.priceSale}</h5>
-                                  <h6>Rs.{item.price}</h6>
-                                  <button
-                                    className="collection-btn"
-                                    style={{ padding: "0px 30px" }}
-                                  >
-                                    Buy Now
-                                  </button>
-                                </span>
+              <div className="row card-container">
+                {productData.map((item, index) => {
+                  return (
+                    <>
+                      <div className="col-lg-4 col-md-8 col-sm-10 image-container">
+                        <div className="card-inner-container">
+                          <Link href={`/product/${item._id}`}>
+                            <img src={`${host}resources/${item.image}`} />
+                            {/* <img src="/resource/images/product-images/angle.jpeg" /> */}
+                            <span>
+                              <p className="tshirt-price">
+                                {item.title} - {item.categoryName}
+                              </p>
+                              <span id="price">
+                                <h5>Rs.{item.priceSale}</h5>
+                                <h6>Rs.{item.price}</h6>
+                                <button
+                                  className="collection-btn"
+                                  style={{ padding: "0px 30px" }}
+                                >
+                                  Buy Now
+                                </button>
                               </span>
-                            </Link>
-                          </div>
+                            </span>
+                          </Link>
                         </div>
-                      </>
-                    );
-                  })}
-                </div>
-              )}
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </>
           )}
         </div>
