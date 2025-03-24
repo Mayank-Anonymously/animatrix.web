a5b3c51e0e4b8c21c2b74cf337092b9cb3bfa959
Mayank-Anonymously <craftaar0@gmail.com>
2025-03-24 18:23:30 +0530

    adde changes

--- pages/checkout/payment/orderid.js
+++ pages/checkout/payment/orderid.js
@@ -1,68 +1,76 @@
-import CreatePayment from "components/api/CreatePayment";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { host } from "static";
+// import CreatePayment from "components/api/CreatePayment";
+// import { useRouter } from "next/router";
+// import React, { useEffect, useState } from "react";
+// import { host } from "static";
 
-const Payment = (props) => {
-  const { orderid } = props;
-  const router = useRouter();
-  const [getPaymentDetails, setPaymentDetails] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [errorText, setErrorText] = useState("");
-  const OrderByID = async () => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+// const Payment = (props) => {
+//   const { orderid } = props;
+//   const router = useRouter();
+//   const [getPaymentDetails, setPaymentDetails] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [errorText, setErrorText] = useState("");
+//   const OrderByID = async () => {
+//     var requestOptions = {
+//       method: "GET",
+//       redirect: "follow",
+//     };
 
-    setLoading(true);
-    const apifetch = await fetch(
-      `${host}order/getOrderByOrderId/${orderid}`,
-      requestOptions
-    );
-    const response = await apifetch.json();
-    if (response) {
-      const data = response.response;
-      CreatePayment({ data, orderid, setPaymentDetails, router });
-      setLoading(false);
-      setErrorText("");
-    } else {
-      setLoading(false);
-      setErrorText("Some Error Occured While Fetching Data");
-    }
-  };
+//     setLoading(true);
+//     const apifetch = await fetch(
+//       `${host}order/getOrderByOrderId/${orderid}`,
+//       requestOptions
+//     );
+//     const response = await apifetch.json();
+//     if (response) {
+//       const data = response.response;
+//       CreatePayment({ data, orderid, setPaymentDetails, router });
+//       setLoading(false);
+//       setErrorText("");
+//     } else {
+//       setLoading(false);
+//       setErrorText("Some Error Occured While Fetching Data");
+//     }
+//   };
 
-  useEffect(() => {
-    OrderByID();
-  }, []);
-  // const defaultOptions = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: redirectload,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+//   useEffect(() => {
+//     OrderByID();
+//   }, []);
+//   // const defaultOptions = {
+//   //   loop: true,
+//   //   autoplay: true,
+//   //   animationData: redirectload,
+//   //   rendererSettings: {
+//   //     preserveAspectRatio: "xMidYMid slice",
+//   //   },
+//   // };
 
-  const Redirectloader = () => {
-    return (
-      <div className="container-fluid justify-content-center align-items-center text-center">
-        {/* <Lottie options={defaultOptions} width={400} height={300} /> */}
-        <h3>Wait While We Redirect You To Payment Page.</h3>
-      </div>
-    );
-  };
+//   const Redirectloader = () => {
+//     return (
+//       <div className="container-fluid justify-content-center align-items-center text-center">
+//         {/* <Lottie options={defaultOptions} width={400} height={300} /> */}
+//         <h3>Wait While We Redirect You To Payment Page.</h3>
+//       </div>
+//     );
+//   };
 
-  return (
-    <div className="container d-flex justify-content-center align-items-center">
-      {errorText === "" ? <Redirectloader /> : errorText}
-    </div>
-  );
+//   return (
+//     <div className="container d-flex justify-content-center align-items-center">
+//       {errorText === "" ? <Redirectloader /> : errorText}
+//     </div>
+//   );
+// };
+
+// export const getServerSideProps = async (context) => {
+//   const { orderid } = context.query;
+
+//   return { props: { orderid: orderid } };
+// };
+// export default Payment;
+
+import React from "react";
+
+const orderid = () => {
+  return <div>orderid</div>;
 };
 
-export const getServerSideProps = async (context) => {
-  const { orderid } = context.query;
-
-  return { props: { orderid: orderid } };
-};
-export default Payment;
+export default orderid;
