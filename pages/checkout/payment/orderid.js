e5774ed4eeb45f539e371e9913a2389ed570d2db
Mayank-Anonymously <craftaar0@gmail.com>
2025-03-24 18:19:52 +0530

    added changes and resolved conflicts

--- pages/checkout/payment/orderid.js
+++ pages/checkout/payment/orderid.js
@@ -1,11 +1,8 @@
-import axios from "axios";
 import CreatePayment from "components/api/CreatePayment";
-import GetOrderByOrderId from "components/api/GetOrderByOrderId";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import Lottie from "react-lottie";
 import { host } from "static";
-import redirectload from "../../../animation/redirect.json";
+
 const Payment = (props) => {
   const { orderid } = props;
   const router = useRouter();
@@ -38,19 +35,19 @@ const Payment = (props) => {
   useEffect(() => {
     OrderByID();
   }, []);
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: redirectload,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: redirectload,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
 
   const Redirectloader = () => {
     return (
       <div className="container-fluid justify-content-center align-items-center text-center">
-        <Lottie options={defaultOptions} width={400} height={300} />
+        {/* <Lottie options={defaultOptions} width={400} height={300} /> */}
         <h3>Wait While We Redirect You To Payment Page.</h3>
       </div>
     );
